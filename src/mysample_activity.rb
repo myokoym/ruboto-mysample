b11a146827f7ec89786c1429856e974fb73be9c3
Masafumi Yokoyama <myokoym@gmail.com>
2013-08-31 17:17:48 +0900

    Fold long line

--- src/mysample_activity.rb
+++ src/mysample_activity.rb
@@ -12,9 +12,15 @@ class MysampleActivity
 
     self.content_view =
         linear_layout(:orientation => :vertical) do
-          @text_view = text_view(:text => "What hath Matz wrought?", :id => 42, :width => :match_parent,
-                                 :gravity => :center, :text_size => 48.0)
-          button(:text => "M-x butterfly", :width => :match_parent, :id => 43, :on_click_listener => proc { butterfly })
+          @text_view = text_view(:text => "What hath Matz wrought?",
+                                 :id => 42,
+                                 :width => :match_parent,
+                                 :gravity => :center,
+                                 :text_size => 48.0)
+          button(:text => "M-x butterfly",
+                 :width => :match_parent,
+                 :id => 43,
+                 :on_click_listener => proc { butterfly })
         end
   rescue
     puts "Exception creating activity: #{$!}"
