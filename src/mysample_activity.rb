b7d83005b7838c699d307050847484439657be27
Masafumi Yokoyama <myokoym@gmail.com>
2013-08-31 16:58:57 +0900

    Add missing parenthesis

--- src/mysample_activity.rb
+++ src/mysample_activity.rb
@@ -8,13 +8,13 @@ ruboto_import_widgets :Button, :LinearLayout, :TextView
 class MysampleActivity
   def onCreate(bundle)
     super
-    set_title "Domo arigato, Mr Ruboto!"
+    set_title("Domo arigato, Mr Ruboto!")
 
     self.content_view =
-        linear_layout :orientation => :vertical do
-          @text_view = text_view :text => "What hath Matz wrought?", :id => 42, :width => :match_parent,
-                                 :gravity => :center, :text_size => 48.0
-          button :text => "M-x butterfly", :width => :match_parent, :id => 43, :on_click_listener => proc { butterfly }
+        linear_layout(:orientation => :vertical) do
+          @text_view = text_view(:text => "What hath Matz wrought?", :id => 42, :width => :match_parent,
+                                 :gravity => :center, :text_size => 48.0)
+          button(:text => "M-x butterfly", :width => :match_parent, :id => 43, :on_click_listener => proc { butterfly })
         end
   rescue
     puts "Exception creating activity: #{$!}"
