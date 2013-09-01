7f46af579766e998430ba2765daa43772260977f
Masafumi Yokoyama <myokoym@gmail.com>
2013-09-01 17:22:04 +0900

    Indent

--- src/mysample_activity.rb
+++ src/mysample_activity.rb
@@ -18,17 +18,17 @@ class MysampleActivity
 
   private
   def top_layout
-        linear_layout(:orientation => :vertical) do
-          @text_view = text_view(:text => "Take anything you would like.",
-                                 :id => 42,
-                                 :width => :match_parent,
-                                 :gravity => :center,
-                                 :text_size => 20.0)
-          button(:text => "Hello, World!",
-                 :width => :match_parent,
-                 :id => 43,
-                 :on_click_listener => proc { hello })
-        end
+    linear_layout(:orientation => :vertical) do
+      @text_view = text_view(:text => "Take anything you would like.",
+                             :id => 42,
+                             :width => :match_parent,
+                             :gravity => :center,
+                             :text_size => 20.0)
+      button(:text => "Hello, World!",
+             :width => :match_parent,
+             :id => 43,
+             :on_click_listener => proc { hello })
+    end
   end
 
   def hello
